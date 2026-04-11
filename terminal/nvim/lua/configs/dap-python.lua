8e0bd4953b2e1de0e85c8827643fe1fb7c1a3dfb
Cody Sheridan <cody.sheridan@proton.me>
2026-04-11 05:20:14 -0400

    debug python az func

--- terminal/nvim/lua/configs/dap-python.lua
+++ terminal/nvim/lua/configs/dap-python.lua
@@ -1,38 +1,29 @@
 local dap = require("dap")
-require("dap-python").setup("./.venv/bin/python") -- fallback, per-project can override
+require("dap-python").setup("./.venv/bin/python")
 
--- Azure Functions debugging configurations
-table.insert(dap.configurations.python, {
-    type = "python",
-    request = "attach",
-    name = "Azure Functions - Attach",
-    connect = {
-        host = "localhost",
-        port = 5678,
-    },
-    pathMappings = {
-        {
-            localRoot = "${workspaceFolder}",
-            remoteRoot = ".",
+dap.adapters.python = {
+    type = "server",
+    host = "127.0.0.1",
+    port = 5678,
+}
+
+--
+-- To start Python az func
+-- export FUNCTIONS_WORKER_PROCESS_COUNT=1
+-- export languageWorkers__python__arguments="-m debugpy --listen 5678 --wait-for-client"
+-- func start
+--
+-- Then start dap in file
+--
+dap.configurations.python = {
+    {
+        type = "python",
+        request = "attach",
+        name = "Attach Azure Functions",
+        connect = {
+            host = "127.0.0.1",
+            port = 5678,
         },
+        justMyCode = false,
     },
-    justMyCode = false,
-})
-
-table.insert(dap.configurations.python, {
-    type = "python",
-    request = "launch",
-    name = "Azure Functions - Launch & Debug",
-    program = "func",
-    args = { "start" },
-    console = "integratedTerminal",
-    cwd = "${workspaceFolder}",
-    justMyCode = false,
-    env = {},
-})
-
--- look for a project-local dap config
-local project_dap = vim.fn.getcwd() .. "/.nvim/dap.lua"
-if vim.fn.filereadable(project_dap) == 1 then
-    dofile(project_dap)
-end
+}
