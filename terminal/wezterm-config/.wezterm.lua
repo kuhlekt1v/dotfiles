bf63bef0be4f6055110797c131836661f798e9a6
Cody Sheridan <cody.sheridan@prattwhitney.com>
2026-09-04 18:14:13 -0400

    [nvim-upgrade] update wezterm config

--- terminal/wezterm-config/.wezterm.lua
+++ terminal/wezterm-config/.wezterm.lua
@@ -1,18 +1,23 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
-local use_lua_colors = true 
+local use_lua_colors = true
 local mux = wezterm.mux
 
 local config = {}
 
--- Start in fullscreen
-wezterm.on("gui-startup", function(cmd)
-	local _, _, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+wezterm.on("toggle-decorations", function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+
+  if not overrides.window_decorations or overrides.window_decorations == "TITLE|RESIZE" then
+    overrides.window_decorations = "RESIZE"
+  else
+    overrides.window_decorations = "TITLE|RESIZE"
+  end
+
+  window:set_config_overrides(overrides)
 end)
 
--- Appearance
-config.font = wezterm.font({ family = "Lilex Nerd Font Mono", weight = "Regular" })
+config.font = wezterm.font({ family = "JetBrainsMono Nerd Font", weight = "Medium" })
 config.show_new_tab_button_in_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_frame = { font_size = 13.0 }
@@ -20,20 +25,35 @@ config.window_decorations = "RESIZE"
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.window_padding = {
-	top = 10,
-	bottom = 10,
-	left = 10,
-	right = 10,
+  top = 10,
+  bottom = 10,
+  left = 10,
+  right = 10,
 }
 config.animation_fps = 1
-config.font_size = 18
+config.font_size = 13
 
 -- Choose lua or toml color scheme
 if use_lua_colors then
-	config.colors = require("colors.nordic")
+  config.colors = require("colors.nordic")
 else
-	config.color_scheme_dirs = { wezterm.config_dir .. "/colors" }
-	config.color_scheme = "jellybeans-dark"
+  config.color_scheme_dirs = { wezterm.config_dir .. "/colors" }
+  config.color_scheme = "jellybeans-dark"
+end
+
+-- WSL
+if wezterm.target_triple:find("windows") then
+  config.wsl_domains = {
+    {
+      name = "WSL:Ubuntu",
+      distribution = "Ubuntu",
+      default_cwd = "~",
+    },
+  }
+
+  config.default_domain = "WSL:Ubuntu"
+else
+  config.default_domain = "local"
 end
 
 -- Leader key (tmux style)
@@ -41,35 +61,32 @@ config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 
 -- Pane navigation
 config.keys = {
+  { key = "h", mods = "CTRL",       action = act.ActivatePaneDirection("Left") },
+  { key = "j", mods = "CTRL",       action = act.ActivatePaneDirection("Down") },
+  { key = "k", mods = "CTRL",       action = act.ActivatePaneDirection("Up") },
+  { key = "l", mods = "CTRL",       action = act.ActivatePaneDirection("Right") },
 
-	{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
-	{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
-	{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
-	{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+  { key = "h", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Left", 5 }) },
+  { key = "j", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Down", 5 }) },
+  { key = "k", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) },
+  { key = "l", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Right", 5 }) },
 
-	{ key = "h", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Left", 5 }) },
-	{ key = "j", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Down", 5 }) },
-	{ key = "k", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) },
-	{ key = "l", mods = "CTRL|SHIFT", action = act.AdjustPaneSize({ "Right", 5 }) },
+  { key = "v", mods = "LEADER",     action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "s", mods = "LEADER",     action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
-	{ key = "v", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "s", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "m", mods = "LEADER",     action = act.TogglePaneZoomState },
+  { key = "c", mods = "LEADER",     action = act.SpawnTab("CurrentPaneDomain") },
 
-	{ key = "m", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "w", mods = "LEADER",     action = act.CloseCurrentPane({ confirm = true }) },
+  { key = "x", mods = "LEADER",     action = act.CloseCurrentTab({ confirm = true }) },
 
-	{ key = "w", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
-	{ key = "x", mods = "LEADER", action = act.CloseCurrentTab({ confirm = true }) },
+  { key = "p", mods = "LEADER",     action = act.ActivateTabRelative(-1) },
+  { key = "n", mods = "LEADER",     action = act.ActivateTabRelative(1) },
 
-	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
-	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
-
-	{ key = "r", mods = "ALT", action = act.ReloadConfiguration },
+  { key = "r", mods = "ALT",        action = act.ReloadConfiguration },
+  { key = "f", mods = "CTRL|SHIFT", action = act.ToggleFullScreen },
+  { key = "c", mods = "CTRL|SHIFT", action = act.SendString("\x0c") },
+  { key = "b", mods = "CTRL|SHIFT", action = act.EmitEvent("toggle-decorations") },
 }
-
-if not wezterm.target_triple:find("windows") then
-	table.insert(config.keys, { key = "f", mods = "CMD|SHIFT", action = wezterm.action.ToggleFullScreen })
-	table.insert(config.keys, { key = "c", mods = "CMD|SHIFT", action = act.SendString("\x0c") })
-end
 
 return config
